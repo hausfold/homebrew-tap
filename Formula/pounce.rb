aa94d4fbb2fb8bb64c9ece22047f6a54739b418b
Julien Martel <julienbmartel@gmail.com>
2026-07-24 06:28:39 -0500

    pounce: revise formula for service fix

--- Formula/pounce.rb
+++ Formula/pounce.rb
@@ -5,6 +5,7 @@ class Pounce < Formula
   url "https://github.com/nebelhaus/pounce/releases/download/v#{version}/pounce-v#{version}-macos.tar.gz"
   sha256 "eff706e73b867b5f0b22484c7ee77b9ddc6aa75be79b6bf2391641e4ec1b248e"
   license "MIT"
+  revision 1
 
   # The version/sha256 lines above are CI-owned: pounce's release workflow
   # rewrites them on every date-versioned tag (nebelhaus/pounce, release.yml) and
